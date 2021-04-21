06740b6b86c4dcbf3309a44c19bce6b4b332aa4e
Tyler <tystuyfzand@gmail.com>
2021-04-20 22:37:38 -0400

    Change to gzip compression

--- packaging/build-package.sh
+++ packaging/build-package.sh
@@ -1,7 +1,7 @@
 fpm -s dir -t deb -p /build/$ARCH/owapi_${VERSION}_${ARCH}.deb \
     -n ow-api -v $VERSION -a $ARCH \
     --deb-priority optional --force \
-    --deb-compression bzip2 \
+    --deb-compression gzip \
     --description "Overwatch API Server" \
     -m "Tyler Stuyfzand <admin@meow.tf>" --vendor "Meow.tf" \
     --before-install packaging/scripts/preinst.deb \
