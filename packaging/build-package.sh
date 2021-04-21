8b8b8186516756bf9416fd6265217a6b3f99f641
Tyler <tystuyfzand@gmail.com>
2021-04-20 22:54:22 -0400

    Proper compression identifier

--- packaging/build-package.sh
+++ packaging/build-package.sh
@@ -1,7 +1,7 @@
 fpm -s dir -t deb -p /build/$ARCH/owapi_${VERSION}_${ARCH}.deb \
     -n ow-api -v $VERSION -a $ARCH \
     --deb-priority optional --force \
-    --deb-compression gzip \
+    --deb-compression gz \
     --description "Overwatch API Server" \
     -m "Tyler Stuyfzand <admin@meow.tf>" --vendor "Meow.tf" \
     --before-install packaging/scripts/preinst.deb \
